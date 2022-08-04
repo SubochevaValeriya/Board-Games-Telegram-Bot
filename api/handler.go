55b8b37080ba234e28b08e84b11b8241bb82ab5f
ValeriyaSubocheva <subocheva.valeria@gmail.com>
2022-08-04 20:39:23 +0300

    Code was added

--- api/handler.go
+++ api/handler.go
@@ -1,29 +1,110 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"net/url"
 	"pwd/internal"
+	"time"
 )
 
-//func Handler(w http.ResponseWriter, r *http.Request) {
-//	defer r.Body.Close()
-//	body, _ := io.ReadAll(r.Body)
-//	var update tgbotapi.Update
-//	if err := json.Unmarshal(body, &update); err != nil {
-//		log.Fatal("Error en el update →", err)
-//	}
-//	log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-//
-//	msg, _ := json.Marshal(data)
-//	log.Printf("Response %s", string(msg))
-//	w.Header().Add("Content-Type", "application/json")
-//	fmt.Fprintf(w, string(msg))
-//}
+type Hito struct {
+	URI   string
+	Title string
+	fecha time.Time
+}
+
+type Response struct {
+	Msg    string `json:"text"`
+	ChatID int64  `json:"chat_id"`
+	Method string `json:"method"`
+}
+
+var hitos = []Hito{
+	Hito{
+		URI:   "0.Repositorio",
+		Title: "Datos básicos y repo",
+		fecha: time.Date(2020, time.September, 29, 11, 30, 0, 0, time.UTC),
+	},
+	Hito{
+		URI:   "1.Infraestructura",
+		Title: "HUs y entidad principal",
+		fecha: time.Date(2020, time.October, 6, 11, 30, 0, 0, time.UTC),
+	},
+	Hito{
+		URI:   "2.Tests",
+		Title: "Tests iniciales",
+		fecha: time.Date(2020, time.October, 16, 11, 30, 0, 0, time.UTC),
+	},
+	Hito{
+		URI:   "3.Contenedores",
+		Title: "Contenedores",
+		fecha: time.Date(2020, time.October, 26, 11, 30, 0, 0, time.UTC),
+	},
+	Hito{
+		URI:   "4.CI",
+		Title: "Integración continua",
+		fecha: time.Date(2020, time.November, 6, 23, 59, 0, 0, time.UTC),
+	},
+	Hito{
+		URI:   "5.Serverless",
+		Title: "Trabajando con funciones serverless",
+		fecha: time.Date(2020, time.November, 24, 11, 30, 0, 0, time.UTC),
+	},
+}
 
 func Handler(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	body, _ := ioutil.ReadAll(r.Body)
+	var update tgbotapi.Update
+	if err := json.Unmarshal(body, &update); err != nil {
+		log.Fatal("Error en el update →", err)
+	}
+	log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+	currentTime := time.Now()
+	var next int
+	var queda time.Duration
+	for indice, hito := range hitos {
+		if hito.fecha.After(currentTime) {
+			next = indice
+			queda = hito.fecha.Sub(currentTime)
+		}
+	}
+	if update.Message.IsCommand() {
+		text := ""
+		if next == 0 {
+			text = "Ninguna entrega próxima"
+		} else {
+
+			switch update.Message.Command() {
+			case "kk":
+				text = queda.String()
+			case "kekeda":
+				text = fmt.Sprintf("→ Próximo hito %s\n🔗 https://jj.github.io/IV/documentos/proyecto/%s\n📅 %s",
+					hitos[next].Title,
+					hitos[next].URI,
+					hitos[next].fecha.String(),
+				)
+			default:
+				text = "Usa /kk para lo que queda para el próximo hito, /kekeda para + detalle"
+			}
+		}
+		data := Response{Msg: text,
+			Method: "sendMessage",
+			ChatID: update.Message.Chat.ID}
+
+		msg, _ := json.Marshal(data)
+		log.Printf("Response %s", string(msg))
+		w.Header().Add("Content-Type", "application/json")
+		fmt.Fprintf(w, string(msg))
+	}
+}
+
+func t(w http.ResponseWriter, r *http.Request) {
 	//	token := mustToken()
 
 	//bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_APITOKEN"))
