b7eae99136d093312f89460ba1cff067c193e756
ValeriyaSubocheva <subocheva.valeria@gmail.com>
2022-08-04 21:38:42 +0300

    Code was added

--- api/handler.go
+++ api/handler.go
@@ -1,45 +1,44 @@
 package handler
 
 import (
-	"encoding/json"
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-	"io/ioutil"
-	"log"
+	"fmt"
 	"net/http"
 )
 
-var bot *tgbotapi.BotAPI
-
-func init() {
-	var err error
-	bot, err = tgbotapi.NewBotAPI("5501151328:AAFVVneFN6O4SLihdwM3qdOTxHmY8mtvNtQ")
-	if err != nil {
-		log.Panic(err)
-	}
-}
+//var bot *tgbotapi.BotAPI
+//
+//func init() {
+//	var err error
+//	bot, err = tgbotapi.NewBotAPI("5501151328:AAFVVneFN6O4SLihdwM3qdOTxHmY8mtvNtQ")
+//	if err != nil {
+//		log.Panic(err)
+//	}
+//}
 
 func Repeater(w http.ResponseWriter, r *http.Request) {
-	defer r.Body.Close()
+	fmt.Println("Hello")
 
-	body, _ := ioutil.ReadAll(r.Body)
-
-	var update tgbotapi.Update
-
-	err := json.Unmarshal(body, &update)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	if update.Message.Text != "" {
-		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-		_, err := bot.Send(msg)
-		if err != nil {
-			log.Println(err)
-		}
-	}
+	//defer r.Body.Close()
+	//
+	//body, _ := ioutil.ReadAll(r.Body)
+	//
+	//var update tgbotapi.Update
+	//
+	//err := json.Unmarshal(body, &update)
+	//if err != nil {
+	//	log.Println(err)
+	//	return
+	//}
+	//
+	//if update.Message.Text != "" {
+	//	log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+	//
+	//	msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+	//	_, err := bot.Send(msg)
+	//	if err != nil {
+	//		log.Println(err)
+	//	}
+	//}
 }
 
 //func t(w http.ResponseWriter, r *http.Request) {
