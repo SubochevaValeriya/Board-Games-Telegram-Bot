e6af133428466ced2ee45fee756d6d322b9b99fa
ValeriyaSubocheva <subocheva.valeria@gmail.com>
2022-08-09 15:31:00 +0300

    Code was added

--- api/handler.go
+++ api/handler.go
@@ -3,11 +3,10 @@ package api
 import (
 	"encoding/json"
 	"github.com/go-telegram-bot-api/telegram-bot-api/v5"
-	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"pwd/clients/telegram"
 )
 
 var bot *tgbotapi.BotAPI
@@ -20,10 +19,10 @@ func init() {
 	}
 }
 
-func Handler(w http.ResponseWriter, r *http.Request) {
+func Repeater(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 
-	body, _ := io.ReadAll(r.Body)
+	body, _ := ioutil.ReadAll(r.Body)
 
 	var update tgbotapi.Update
 
@@ -36,6 +35,53 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	if update.Message.Text != "" {
 		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 
-		telegram.SendMsg(telegram.ReceiveRequest(bot), bot)
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		_, err := bot.Send(msg)
+		if err != nil {
+			log.Println(err)
+		}
 	}
 }
+
+//
+//package api
+//
+//import (
+//	"encoding/json"
+//	"github.com/go-telegram-bot-api/telegram-bot-api/v5"
+//	"io"
+//	"log"
+//	"net/http"
+//	"os"
+//	"pwd/clients/telegram"
+//)
+//
+//var bot *tgbotapi.BotAPI
+//
+//func init() {
+//	var err error
+//	bot, err = tgbotapi.NewBotAPI(os.Getenv("BOT_TOKEN"))
+//	if err != nil {
+//		log.Panic(err)
+//	}
+//}
+//
+//func Handler(w http.ResponseWriter, r *http.Request) {
+//	defer r.Body.Close()
+//
+//	body, _ := io.ReadAll(r.Body)
+//
+//	var update tgbotapi.Update
+//
+//	err := json.Unmarshal(body, &update)
+//	if err != nil {
+//		log.Println(err)
+//		return
+//	}
+//
+//	if update.Message.Text != "" {
+//		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+//
+//		telegram.SendMsg(telegram.ReceiveRequest(bot), bot)
+//	}
+//}
