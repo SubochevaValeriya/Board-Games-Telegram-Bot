fa10b32deba7bc3f50d80f47e9e90159a2a03026
ValeriyaSubocheva <subocheva.valeria@gmail.com>
2022-08-09 13:24:37 +0300

    Code was added

--- clients/telegram/receiver.go
+++ clients/telegram/receiver.go
@@ -5,17 +5,10 @@ import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 func ReceiveRequest(bot *tgbotapi.BotAPI) tgbotapi.UpdatesChannel {
 	bot.Debug = true
 
-	// Create a new UpdateConfig struct with an offset of 0. Offsets are used
-	// to make sure Telegram knows we've handled previous values and we don't
-	// need them repeated.
 	updateConfig := tgbotapi.NewUpdate(0)
 
-	// Tell Telegram we should wait up to 30 seconds on each request for an
-	// update. This way we can get information just as quickly as making many
-	// frequent requests without having to send nearly as many.
 	updateConfig.Timeout = 30
 
-	// Start polling Telegram for updates.
 	updates := bot.GetUpdatesChan(updateConfig)
 	return updates
 }
