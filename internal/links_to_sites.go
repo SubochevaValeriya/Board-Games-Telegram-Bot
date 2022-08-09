07b24def28c5e72806dd64b0e48428e79e9cea18
ValeriyaSubocheva <subocheva.valeria@gmail.com>
2022-08-09 18:04:43 +0300

    Code was added

--- internal/links_to_sites.go
+++ internal/links_to_sites.go
@@ -12,7 +12,7 @@ import (
 
 func (g *GameInfo) TeseraLinkM(s string) {
 	log.Println(s)
-	urlSearch := "https://api.tesera.ru/search/games?query=%" + s + "&withAdditions=false&WaitHandle.Handle=%7B%7D%22%20/"
+	urlSearch := "https://api.tesera.ru/search/games?query=%%22%20" + s + "%20s%20+%20\"&withAdditions=false&WaitHandle.Handle=%7B%7D\"%20/"
 	response, err := http.Get(urlSearch)
 	//response, err := http.Get("https://api.tesera.ru/search/games?query=%%22%20%D0%B1%D1%8D%D0%BD%D0%B3%20s%20+%20%22&withAdditions=false&WaitHandle.Handle=%7B%7D%22%20/")
 
