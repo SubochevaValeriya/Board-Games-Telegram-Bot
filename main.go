1b095a98defaac339b4ffbb3839d2a71df3c2909
ValeriyaSubocheva <subocheva.valeria@gmail.com>
2022-08-10 21:04:58 +0300

    Revert "code update"
    This reverts commit 2aab17d7d48fac57f1799d5f4111c9d3cb3d5da0.

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"os"
+	"pwd/clients/telegram"
+)
+
 func main() {
 	// The code implements deployment using serverless function and webhooks (pls see api/handler.go)
 	// In the main function I left the method using the long polling (all that is needed is to add a loop and change the Update type to UpdateChannels in the SendMsg function (sender.go))
 
-	//bot, err := tgbotapi.NewBotAPI(os.Getenv("BOT_TOKEN"))
-	//if err != nil {
-	//	panic(err)
-	//}
-	//
-	//telegram.SendMsg(telegram.ReceiveRequest(bot), bot)
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("BOT_TOKEN"))
+	if err != nil {
+		panic(err)
+	}
+
+	telegram.SendMsg(telegram.ReceiveRequest(bot), bot)
 }
